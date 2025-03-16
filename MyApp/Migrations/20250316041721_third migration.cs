37cbdc40652631121bf7bec0c0199617f20e21df
Feihuan Peng <ericrealtor1997@gmail.com>
2025-03-16 15:48:41 -0700

    Add project files.

---
+++ MyApp/Migrations/20250316041721_third migration.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class thirdmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SeriaNumberId",
+                table: "Items",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "SeriaNumbers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SeriaNumbers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SeriaNumbers_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Items",
+                columns: new[] { "Id", "Name", "Price", "SeriaNumberId" },
+                values: new object[] { 4, "Microphone", 40.0, 10 });
+
+            migrationBuilder.InsertData(
+                table: "SeriaNumbers",
+                columns: new[] { "Id", "ItemId", "Name" },
+                values: new object[] { 10, 4, "MIC150" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SeriaNumbers_ItemId",
+                table: "SeriaNumbers",
+                column: "ItemId",
+                unique: true,
+                filter: "[ItemId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SeriaNumbers");
+
+            migrationBuilder.DeleteData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DropColumn(
+                name: "SeriaNumberId",
+                table: "Items");
+        }
+    }
+}
---
+++ MyApp/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿// Add-Migration "first migration"
+//Update-Database
+// Add-Migration "second migration"
+//Update-Database
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyApp.Models
+{
+    public class Item
+
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public double Price { get; set; }
+
+        // one to one
+        public int? SeriaNumberId { get; set; }
+        public SeriaNumber? SeriaNumber { get; set; }
+
+        public int? CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+
+        public List<ItemClient>? ItemClients { get; set; }
+    }
+    //   /item/overview  controller class name, action method is after
+}
